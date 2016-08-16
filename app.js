06cbab547cce1aaa9d2a0eaf05e679e8b3a17a08
yossale <yossale.pepper@gmail.com>
2016-08-16 17:07:46 +0300

    first step

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
 (function() {
-
-  var AppComponent = ng.core.Component({
-    selector: 'my-app',
-    template: '<h1>Hello World!</h1>'
-  })
-  .Class({
-    constructor: function() {
-      // empty
-    }
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(AppComponent);
-  });
-
 })();
